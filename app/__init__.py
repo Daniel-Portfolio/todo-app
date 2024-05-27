ac8ec51563418bb9f5e117931d5673b4b6b4119b
Daniel Rashba <danirdd92@gmail.com>
2024-05-27 14:25:33 +0300

    db connection

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import dotenv_values
+
+config = dotenv_values(".env.dev")
+
+app = Flask(__name__)
+app.config["SECRET"] = config["SECRET"]
+
+app.config["MONGO_URI"] = f"mongodb://{config['MONGO_USER']
+                                       }:{config['MONGO_PWD']}@127.0.0.1:27017/todo"
+
+
+client = PyMongo(app)
+db = client.db
+
+from app import routes

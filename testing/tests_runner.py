a420b037bbc0bbbccc07014102b9fc22a5e88956
Daniel Rashba <danirdd92@gmail.com>
2024-05-27 17:13:48 +0300

    added dummy unit tests

---
+++ testing/tests_runner.py
@@ -0,0 +1,61 @@
+import pytest
+
+
+def tests_suite():
+    assert 1 == 1
+
+
+def tests_suite_2():
+    assert 2 == 2
+
+
+def tests_suite_3():
+    assert 3 == 3
+
+
+def tests_suite_4():
+    assert 4 == 4
+
+
+def tests_suite_5():
+    assert 5 == 5
+
+
+def tests_suite_6():
+    assert 6 == 6
+
+
+def tests_suite_7():
+    assert 7 == 7
+
+
+def tests_suite_8():
+    assert 8 == 8
+
+
+def tests_suite_9():
+    assert 9 == 9
+
+
+def tests_suite_0():
+    assert 10 == 10
+
+
+def tests_suite_1():
+    assert 11 == 11
+
+
+def tests_suite_2():
+    assert 12 == 12
+
+
+def tests_suite_3():
+    assert 13 == 13
+
+
+def tests_suite_4():
+    assert 14 == 14
+
+
+def tests_suite_5():
+    assert 15 == 15

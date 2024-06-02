2aaa3bcab19a5dea62d269693974ab405edd1dae
Daniel Rashba <danirdd92@gmail.com>
2024-06-02 19:01:52 +0300

    add metrics to flask app

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from app import app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')

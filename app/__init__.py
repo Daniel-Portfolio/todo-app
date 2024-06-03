200b18618ead501d01f4ff640761fb53287f578a
Daniel Rashba <danirdd92@gmail.com>
2024-06-03 09:09:36 +0300

    add info logs

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from pymongo import MongoClient
 from os import getenv
+import logging
 
 MONGO_URI = getenv("MONGO_URI")
 SECRET_KEY = getenv("SECRET_KEY")
@@ -8,6 +9,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 app = Flask(__name__)
 app.config["SECRET_KEY"] = SECRET_KEY
 app.config["MONGO_URI"] = MONGO_URI
+app.logger.setLevel(logging.INFO)
 
 client = MongoClient(app.config["MONGO_URI"])
 db = client['todos']
